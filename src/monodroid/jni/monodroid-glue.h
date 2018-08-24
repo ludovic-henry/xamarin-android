b0f02706f3322c64a33fa71b3beb563387ad46d7
Ludovic Henry <luhenry@microsoft.com>
2018-08-24 18:36:59 -0400

    {wip} [sdks] Invert loading of libmono and libmonodroid

--- src/monodroid/jni/monodroid-glue.h
+++ src/monodroid/jni/monodroid-glue.h
@@ -13,4 +13,25 @@ struct DylibMono  *monodroid_get_dylib (void);
 extern  FILE  *gref_log;
 extern  FILE  *lref_log;
 
+MONO_API void
+monodroid_runtime_init (JNIEnv*, jclass, jstring, jobjectArray, jstring, jobjectArray, jobject, jobjectArray, jobjectArray, jstring);
+
+MONO_API void
+monodroid_runtime_register (JNIEnv*, jclass, jstring, jclass, jstring);
+
+MONO_API void
+monodroid_runtime_notifyTimeZoneChanged (JNIEnv*, jclass);
+
+MONO_API jint
+monodroid_runtime_createNewContext (JNIEnv*, jclass, jobjectArray, jobjectArray, jobject);
+
+MONO_API void
+monodroid_runtime_switchToContext (JNIEnv*, jclass, jint);
+
+MONO_API void
+monodroid_runtime_destroyContexts (JNIEnv*, jclass, jintArray);
+
+MONO_API void
+monodroid_runtime_propagateUncaughtException (JNIEnv*, jclass, jobject, jthrowable);
+
 #endif /* __MONODROID_GLUE_H */
